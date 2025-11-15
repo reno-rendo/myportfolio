54ecc9baa5230fff16de7a72ee0ecc6d8a3bb683
hafsa-aarifeen-x4 <hafsa.aarifeen@x4digitallabs.com>
2025-11-15 20:26:27 +0530

    Floating side menu

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,9 +1,4 @@
 /**
- * @copyright 2025 codewithsadee
- * @license Apache-2.0
- */
-
-/**
  * Types
  */
 import type {
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import '@/index.css';
 import { App } from './App';
 import { Sidebar } from './components/Sidebar';
+import { FloatingMenu } from './components/FloatingMenu';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className='min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10'>
       <Sidebar />
+      <FloatingMenu />
       <App />
     </div>
   </StrictMode>,
