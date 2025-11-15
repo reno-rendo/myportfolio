ec976e6dad6408b6c8970a77c426901e8f494486
hafsa-aarifeen-x4 <hafsa.aarifeen@x4digitallabs.com>
2025-11-15 17:25:03 +0530

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@/index.css';
+import { App } from './App';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <div className='min-h-screen lg:flex lg:justify-center lg:items-start lg:gap-10'>
+      <App />
+    </div>
+  </StrictMode>,
+);
