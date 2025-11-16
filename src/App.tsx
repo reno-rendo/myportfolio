4b2939eef12ac3f7eef97e24a968def5fc91a351
hafsa-aarifeen-x4 <hafsa.aarifeen@x4digitallabs.com>
2025-11-16 20:07:37 +0530

    Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Button } from './components/ui/button';
+import { useEffect } from 'react';
+import { Hero } from './components/Hero';
 
 export const App = () => {
   return (
-    <div className='h-[-300vh]'>
-      <Button>Hafsa Aarifeen</Button>
-    </div>
+    <main className='flex flex-col container mx-auto p-10 max-w-4xl lg:pr-10 lg:pl-0 lg:max-w-6xl'>
+      <Hero />
+    </main>
   );
 };
