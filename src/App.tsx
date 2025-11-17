5fbef7412a4bc84746e97c4a2b0b3b549c904527
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2025-11-17 19:10:22 +0530

    Project section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
 import { Hero } from './components/Hero';
 import { Stats } from './components/Stats';
+import { Projects } from './components/Projects';
 
 export const App = () => {
   return (
     <main className='flex flex-col container mx-auto p-10 max-w-4xl lg:pr-10 lg:pl-0 lg:max-w-6xl'>
       <Hero />
       <Stats />
+      <Projects />
     </main>
   );
 };
---
+++ src/components/Projects.tsx
@@ -0,0 +1,44 @@
+import { motion } from 'motion/react';
+
+import { staggerContainer } from '@/lib/animations';
+
+import { SectionHeader } from './SectionHeader';
+import { ProjectCard } from './ProjectCard';
+
+import { projectsData } from '@/constants';
+
+export const Projects = () => {
+  return (
+    <motion.section
+      initial='hidden'
+      whileInView='visible'
+      viewport={{ once: true, amount: 0.3 }}
+      variants={staggerContainer(0)}
+      className='mt-20 scroll-mt-10'
+      id='projects'
+    >
+      <SectionHeader
+        subtitle='Projects'
+        title='My Featured Projects'
+      />
+
+      <motion.div
+        initial='hidden'
+        whileInView='visible'
+        viewport={{ once: true, amount: 0.3 }}
+        variants={staggerContainer(0.5)}
+        className='grid md:grid-cols-2 gap-10 mt-10'
+      >
+        {projectsData.map((project, i) => (
+          <ProjectCard
+            key={i}
+            imgSrc={project.imgSrc}
+            projectLink={project.projectLink}
+            tags={project.tags}
+            title={project.title}
+          />
+        ))}
+      </motion.div>
+    </motion.section>
+  );
+};
